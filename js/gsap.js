745b626e9f556d2abe42541d6e63c6b3078b3feb
Jason Nutt <jnutt367@gmail.com>
2021-05-29 17:38:43 -0400

    commit for great intro movie

--- js/gsap.js
+++ js/gsap.js
@@ -1,5 +1,6 @@
 let ginger = gsap.timeline()
 
-.fromTo("h1", { y: -1000, x: 100, opacity: 0 }, { duration: 2, x: 0, ease: "elastic", fontFamily: "fantasy", y: 0, opacity: 1 }, 10);
+.fromTo("h1", { y: -1000, x: 100, opacity: 0 }, { duration: 2, x: 0, ease: "elastic", fontFamily: "fantasy", y: 0, opacity: 1 }, 15)
 
-gsap.fromTo("body", { opacity: 0 }, { opacity: 1 }, 5)
+.fromTo("body", { backgroundColor: "transparent", opacity: 0 }, { duration: 10, backgroundColor: "orange", opacity: 1 }, 2)
+    .to("body", { duration: 6, backgroundColor: "black" }, 11)
