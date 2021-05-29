b681aa95d34ee064aebb64cfe90a4b30879d6826
Jason Nutt <jnutt367@gmail.com>
2021-05-29 17:06:04 -0400

    commit for better

--- js/gsap.js
+++ js/gsap.js
@@ -1,3 +1,5 @@
 let ginger = gsap.timeline()
 
-.fromTo("h1", { y: -200, x: 100, opacity: 0 }, { duration: 2, x: 0, ease: "elastic", fontFamily: "fantasy", y: 0, opacity: 1 }, 10)
+.fromTo("h1", { y: -1000, x: 100, opacity: 0 }, { duration: 2, x: 0, ease: "elastic", fontFamily: "fantasy", y: 0, opacity: 1 }, 10);
+
+gsap.fromTo("body", { opacity: 0 }, { opacity: 1 }, 5)
