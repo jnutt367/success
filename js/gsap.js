cbab027af21596e50217d2bdb1ae914d2f737a13
Jason Nutt <jnutt367@gmail.com>
2021-06-01 15:56:56 -0400

    commit js

--- js/gsap.js
+++ js/gsap.js
@@ -2,7 +2,7 @@ let ginger = gsap.timeline()
 
 .fromTo("h1", { y: -1000, x: 100, opacity: 0 }, { color: "skyblue", duration: 2, x: 0, ease: "elastic", fontSize: "30", fontFamily: "cursive", y: -130, opacity: 1 }, 10)
     .to("li", { duration: 2, scale: 1.2, ease: "elastic", color: "skyblue" })
-    .fromTo("body", { backgroundColor: "transparent", opacity: 0 }, { duration: 10, backgroundColor: "skyblue", opacity: 1 }, 0)
+    .fromTo("body", { backgroundColor: "transparent", opacity: 0 }, { duration: 10, backgroundColor: "black", opacity: 1 }, 0)
     .to("h2", { color: "white", repeat: 3, fontFamily: "serif", borderRadius: "10" })
     .to("body", { duration: 6, backgroundColor: "black" }, 10)
     .fromTo("h3", { x: -300, y: -250 }, { x: 55, y: -270, color: "skyblue" }, 10)
