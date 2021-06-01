fcd79c755945737b05fb07b0f4647050dcaa7be1
Jason Nutt <jnutt367@gmail.com>
2021-06-01 11:37:47 -0400

    commit for js change

--- js/gsap.js
+++ js/gsap.js
@@ -1,6 +1,6 @@
 let ginger = gsap.timeline()
 
-.fromTo("h1", { y: -1000, x: 100, opacity: 0 }, { duration: 2, x: 0, ease: "elastic", fontSize: "30", fontFamily: "Open Sans", y: -100, opacity: 1 }, 10)
+.fromTo("h1", { y: -1000, x: 100, opacity: 0 }, { duration: 2, x: 0, ease: "elastic", fontSize: "30", fontFamily: "Open Sans", y: -130, opacity: 1 }, 10)
     .to("li", { duration: 2, scale: 1.2, ease: "elastic", color: "orange" })
     .fromTo("body", { backgroundColor: "transparent", opacity: 0 }, { duration: 10, backgroundColor: "orange", opacity: 1 }, 0)
     .to("body", { duration: 6, backgroundColor: "black" }, 10)
