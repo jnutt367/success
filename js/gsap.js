88e6591949d036c1588e014f7cb188b1c693862a
Jason Nutt <jnutt367@gmail.com>
2021-06-01 12:31:34 -0400

    commit js

--- js/gsap.js
+++ js/gsap.js
@@ -1,12 +1,12 @@
 let ginger = gsap.timeline()
 
-.fromTo("h1", { y: -1000, x: 100, opacity: 0 }, { duration: 2, x: 0, ease: "elastic", fontSize: "30", fontFamily: "Open Sans", y: -130, opacity: 1 }, 10)
+.fromTo("h1", { y: -1000, x: 100, opacity: 0 }, { color: "orange", duration: 2, x: 0, ease: "elastic", fontSize: "30", fontFamily: "Open Sans", y: -130, opacity: 1 }, 10)
     .to("li", { duration: 2, scale: 1.2, ease: "elastic", color: "orange" })
     .fromTo("body", { backgroundColor: "transparent", opacity: 0 }, { duration: 10, backgroundColor: "orange", opacity: 1 }, 0)
     .to("h2", { color: "white", repeat: -1 })
     .to("body", { duration: 6, backgroundColor: "black" }, 10)
-    .fromTo("h3", { x: -300, y: -250 }, { x: 50, y: -270, color: "orange" }, 7);
-
+    .fromTo("h3", { x: -300, y: -250 }, { x: 55, y: -270, color: "orange" }, 10)
+    .to("h3", { duration: 3, fontFamily: "sans serif" }, 11)
 let title = gsap.timeline()
 
 .to("h1", { duration: 5, ease: "elastic", fontFamily: "cursive" }, 11)
